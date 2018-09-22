fa140dec89cf8650750cc7ee4a33f731a426add9
Ktars <v.manevich.v@icloud.com>
2018-09-23 01:07:06 +0300

    chat

---
+++ src/config.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyCZgfFCJSNpHd1rhTx2HC2rzhxrO7-drZI",
+    authDomain: "chatapp-2fe7b.firebaseapp.com",
+    databaseURL: "https://chatapp-2fe7b.firebaseio.com",
+    projectId: "chatapp-2fe7b",
+    storageBucket: "chatapp-2fe7b.appspot.com",
+    messagingSenderId: "793286729766"
+
+};
